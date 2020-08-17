37d751dd273408bdee2c504c22d7601a2ce3a17b
jtclayt <jt.clayton92@yahoo.com>
2020-08-17 11:48:19 -0700

    Complete the wall assignment

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 using Session;
@@ -23,7 +24,49 @@ namespace Wall.Controllers
             {
                 return RedirectToAction("Login", "User");
             }
-            return View("Index");
+            IndexWrapper data = new IndexWrapper();
+            data.AllMessages = _db.Messages
+                .Include(m => m.Creator)
+                .Include(m => m.Comments)
+                .ThenInclude(c => c.Creator)
+                .OrderByDescending(m => m.CreatedAt)
+                .ToArray();
+            return View("Index", data);
+        }
+
+        [HttpPost("messages")]
+        public IActionResult CreateMessage(IndexWrapper postData)
+        {
+            if (!CheckUser())
+            {
+                return RedirectToAction("Login", "User");
+            }
+            if (ModelState.IsValid)
+            {
+                postData.MessageForm.UserId = HttpContext.Session.Get<User>("user").UserId;
+                _db.Add(postData.MessageForm);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return Index();
+        }
+
+        [HttpPost("messages/{id}")]
+        public IActionResult CreateComment(int id, IndexWrapper postData)
+        {
+            if (!CheckUser())
+            {
+                return RedirectToAction("Login", "User");
+            }
+            if (ModelState.IsValid)
+            {
+                postData.CommentForm.UserId = HttpContext.Session.Get<User>("user").UserId;
+                postData.CommentForm.MessageId = id;
+                _db.Add(postData.CommentForm);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return Index();
         }
 
         private bool CheckUser()
---
+++ Migrations/20200817181747_FixUser.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Wall.Migrations
+{
+    public partial class FixUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Users_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_UserId",
+                table: "Messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Users_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_UserId",
+                table: "Messages",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Users_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_UserId",
+                table: "Messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Users_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_UserId",
+                table: "Messages",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
